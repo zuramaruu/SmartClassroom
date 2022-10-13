2b83cbf61c3905619746fcb78d43174129d4160a
Juramaru <109219513+zuramaruu@users.noreply.github.com>
2022-10-14 02:21:34 +0700

    Delete Board directory

--- Board/Master-thingSpeak/timeCfg.ino
+++
@@ -1,8 +0,0 @@
-void FireTimer_Init()
-{
-  msTimer.begin(3000);
-
-  dariNANOTimer.begin(3000);
-  keNANOTimer.begin(3000);
-  thingSpeakTmr.begin(20000);
-}
--- Board/Master/timeCfg.ino
+++
@@ -1,8 +0,0 @@
-void FireTimer_Init()
-{
-  msTimer.begin(3000);
-
-  dariNANOTimer.begin(1000);
-  keNANOTimer.begin(1000);
-  dariServerTimer.begin(1000);
-}
